fdd705972c112a194565b41f53a3533ba46977f6
Ben Ryan <50218096+benryan03@users.noreply.github.com>
2020-03-14 17:49:58 -0400

    Added CSS

--- form.php
+++ form.php
@@ -1,42 +1,53 @@
 <html>
 <head>
-    <title>Success</title>
+<title>Contact Form</title>
+
+<link rel="stylesheet" type="text/css" href="default.css">
+
 </head>
 <body>
 <center>
 
-<?php
+<div class="header">
+    <h1>Contact Form</h1>
+</div>
 
-$userFirstName = $_POST["userFirstName"];
-$userLastName = $_POST["userLastName"];
-$userEmail = $_POST["userEmail"];
-$userPhoneNumber = $_POST["userPhoneNumber"];
-$userGender = $_POST["userGender"];
+<div class="content">
 
-$serverName = "SERVER_ADDRESS\sqlexpress";
-$connectionInfo = array("Database"=>"DATABASE_NAME_HERE", "UID"=>"USERNAME_HERE", "PWD"=>"PASSWORD_HERE");
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+    <?php
 
-$query = "INSERT INTO People VALUES ('$userFirstName', '$userLastName', '$userEmail', '$userPhoneNumber', '$userGender')";
-$writeToDatabase = sqlsrv_query($conn, $query);
+        $userFirstName = $_POST["userFirstName"];
+        $userLastName = $_POST["userLastName"];
+        $userEmail = $_POST["userEmail"];
+        $userPhoneNumber = $_POST["userPhoneNumber"];
+        $userGender = $_POST["userGender"];
 
-if($conn){
-    #echo "Connection established.<br><br>";
-}
-else{
-    echo "Connection could not be established.<br><br>";
-    die(print_r(sqlsrv_errors(), true));
-}
+        $serverName = "SERVER_ADDRESS\sqlexpress";
+        $connectionInfo = array("Database"=>"DATABASE_NAME_HERE", "UID"=>"USERNAME_HERE", "PWD"=>"PASSWORD_HERE");
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if ($writeToDatabase){
-    echo "Thank you. Your submission has been recorded.";
-}
-else {
-    echo "Error writing to database.<br><br>";
-    die(print_r(sqlsrv_errors(), true));
-}
+        $query = "INSERT INTO People VALUES ('$userFirstName', '$userLastName', '$userEmail', '$userPhoneNumber', '$userGender')";
+        $writeToDatabase = sqlsrv_query($conn, $query);
 
-?>
+        if($conn){
+            #echo "Connection established.<br><br>";
+        }
+        else{
+            echo "Connection could not be established.<br><br>";
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        if ($writeToDatabase){
+            echo "Thank you. Your submission has been recorded.";
+        }
+        else {
+            echo "Error writing to database.<br><br>";
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+    ?>
+
+</div>
 </center>
 </body>
 </html>
