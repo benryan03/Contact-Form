6f226d6249d0eb39625429d5dce6010a36d1d745
Ben Ryan <50218096+benryan03@users.noreply.github.com>
2020-03-15 14:38:28 -0400

    Moved DB write code from form.php to index.php

--- form.php
+++ form.php
@@ -14,45 +14,7 @@
 
 <div class="content">
 
-    <?php
-
-        $userFirstName = $_POST["userFirstName"];
-        $userLastName = $_POST["userLastName"];
-        $userEmail = $_POST["userEmail"];
-        $userPhoneNumber = $_POST["userPhoneNumber"];
-        if ($_POST["userGender"] === "male"){
-            $userGender = "M";
-        }
-        elseif ($_POST["userGender"] === "female"){
-            $userGender = "F";
-        }
-        elseif ($_POST["userGender"] === "other"){
-            $userGender = "X";
-        }
-
-        $serverName = "SERVER_ADDRESS\sqlexpress";
-        $connectionInfo = array("Database"=>"DATABASE_NAME_HERE", "UID"=>"USERNAME_HERE", "PWD"=>"PASSWORD_HERE");
-        $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-        $query = "INSERT INTO People VALUES ('$userFirstName', '$userLastName', '$userEmail', '$userPhoneNumber', '$userGender')";
-        $writeToDatabase = sqlsrv_query($conn, $query);
-
-        if($conn){
-            #echo "Connection established.<br><br>";
-        }
-        else{
-            echo "Connection could not be established.<br><br>";
-            die(print_r(sqlsrv_errors(), true));
-        }
-
-        if ($writeToDatabase){
-            echo "Thank you. Your submission has been recorded.";
-        }
-        else {
-            echo "Error writing to database.<br><br>";
-            die(print_r(sqlsrv_errors(), true));
-        }
-    ?>
+    Thank you. Your reponse has been recorded.
 
 </div>
 </center>
--- index.php
+++ index.php
@@ -33,7 +33,41 @@ if(!empty($_POST["submit"])){ //if submitted, then validate
         $genderError="Please select your gender";
     }
     else{
-        $userGender = $_POST["userGender"];
+        if ($_POST["userGender"] === "male"){
+            $userGender = "M";
+        }
+        elseif ($_POST["userGender"] === "female"){
+            $userGender = "F";
+        }
+        elseif ($_POST["userGender"] === "other"){
+            $userGender = "X";
+        }
+    }
+    if (!empty($userFirstName) && !empty($userLastName) && !empty($userEmail) && !empty($userPhoneNumber) && !empty($userGender)){
+        $serverName = "localhost\sqlexpress";
+        $connectionInfo = array("Database"=>"Test", "UID"=>"ben", "PWD"=>"106627");
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+        $query = "INSERT INTO People VALUES ('$userFirstName', '$userLastName', '$userEmail', '$userPhoneNumber', '$userGender')";
+        $writeToDatabase = sqlsrv_query($conn, $query);
+
+        if($conn){
+            #echo "Connection established.<br><br>";
+        }
+        else{
+            echo "Connection could not be established.<br><br>";
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        if ($writeToDatabase){
+            echo "Thank you. Your submission has been recorded.";
+        }
+        else {
+            echo "Error writing to database.<br><br>";
+            die(print_r(sqlsrv_errors(), true));
+        }
+    
+        header("Location:form.php");
     }
 }
 ?>
@@ -49,23 +83,23 @@ if(!empty($_POST["submit"])){ //if submitted, then validate
 <center>
 
 <div class="header">
-    <h1><a href="index.php">Contact Form</a></h1>
+    <h1>Contact Form</h1>
 </div>
 
 <div class="content">
     Enter your contact details:<br><br>
     <form class="contact-form" action="?" method="post">
 
-        <input type="text" name="userFirstName" placeholder="First name" value="<?php echo htmlentities($userFirstName) ?>"><br>
+        <input type="text" name="userFirstName" placeholder="First name" value="<?php echo htmlentities($userFirstName) ?>">&nbsp;*<br>
         <span class="error"><?php echo "$firstNameError" ?></span><br><br>
 
-        <input type="text" name="userLastName" placeholder="Last name" value="<?php echo htmlentities($userLastName) ?>"><br>
+        <input type="text" name="userLastName" placeholder="Last name" value="<?php echo htmlentities($userLastName) ?>">&nbsp;*<br>
         <span class="error"><?php echo "$lastNameError" ?></span><br><br>
 
-        <input type="text" name="userEmail" placeholder="E-mail address" value="<?php echo htmlentities($userEmail) ?>"><br>
+        <input type="text" name="userEmail" placeholder="E-mail address" value="<?php echo htmlentities($userEmail) ?>">&nbsp;*<br>
         <span class="error"><?php echo "$emailError" ?></span><br><br>
 
-        <input type="text" name="userPhoneNumber" placeholder="Phone number" value="<?php echo htmlentities($userPhoneNumber) ?>"><br>
+        <input type="text" name="userPhoneNumber" placeholder="Phone number" value="<?php echo htmlentities($userPhoneNumber) ?>">&nbsp;*<br>
         <span class="error"><?php echo "$phoneError" ?></span><br><br>
 
         <input type="radio" id="male" name="userGender" value="male" <?php echo ($userGender=="male")? "checked":""; ?> >
@@ -73,7 +107,7 @@ if(!empty($_POST["submit"])){ //if submitted, then validate
         <input type="radio" id="female" name="userGender" value="female" <?php echo ($userGender=="female")? "checked":""; ?> >
         <label for="female">Female</label>
         <input type="radio" id="other" name="userGender" value="other" <?php echo ($userGender=="other")? "checked":""; ?> >
-        <label for="other">Other</label><br>
+        <label for="other">Other</label>&nbsp;*<br>
         <span class="error"><?php echo $genderError ?></span><br><br>
 
         <input type="submit" value="Submit" name="submit">
