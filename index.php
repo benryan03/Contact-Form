bc111469c27cebb8d43212a46421ed5528c74c7b
Ben Ryan <50218096+benryan03@users.noreply.github.com>
2020-03-20 13:07:04 -0400

    Added success page

--- index.php
+++ index.php
@@ -1,120 +1,121 @@
 <?php
-$userFirstName = "";
-$userLastName = "";
-$userPhoneNumber = "";
-$userEmail = "";
+$username = "";
+$password = "";
+$confirmPassword = "";
+$usernameError = "";
+$passwordError = "";
+$confirmPasswordError = "";
+$submitError = "";
+$errorStatus = false;
+date_default_timezone_set("America/New_York");
+$timestamp = date("Y/m/d h:i:sa");
 
-$userGender = "";
+if (!empty($_POST["submit"])){                  //if submit button clicked, then validate inputs
+    $username = trim($_POST["username"]);       //trim() removes whitespace from  both sides of input
+    $username = htmlspecialchars($username);    //convert html characters to html entities
 
-$firstNameError="";
-$lastNameError="";
-$phoneError="";
-$emailError="";
-$genderError = "";
+    if (empty($username)){
+        $usernameError = "Please enter a username.";
+        $errorStatus = true;
+    }
+    elseif (!empty($username)) {
+        //Check if username already exists
 
-if(!empty($_POST["submit"])){ //if submitted, then validate
-    $userFirstName = trim($_POST["userFirstName"], ENT_QUOTES); //trim() removes whitespace from both sides of input
-    $userFirstName = htmlspecialchars($userFirstName); //htmlspecialchars() converts <, >, "", ', and & to HTML entities
-    if (empty($userFirstName)){
-        $firstNameError = "Please enter your first name";
-    }
-    $userLastName = trim($_POST["userLastName"]);
-    $userLastName = htmlspecialchars($userLastName);
-    if (empty($userLastName)){
-        $lastNameError = "Please enter your last name";
-    }
-    $userEmail = trim($_POST["userEmail"]);
-    $userEmail = htmlspecialchars($userEmail);
-    if (empty($userEmail)){
-        $emailError = "Please enter your e-mail address";
-    }
-    if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL)){
-        $emailError = "Please enter a valid e-mail address";        
-    }
-    $userPhoneNumber = trim($_POST["userPhoneNumber"]);
-    $userPhoneNumber = htmlspecialchars($userPhoneNumber);
-    if (empty($userPhoneNumber)){
-        $phoneError = "Please enter your phone number";
-    }
-    if (!isset($_POST["userGender"])){ 
-        $genderError="Please select your gender";
-    }
-    else{
-        $userGender = $_POST["userGender"];
-    }    
-    if (!empty($userFirstName) && !empty($userLastName) && !empty($userEmail) && !empty($userPhoneNumber) && !empty($userGender) && filter_var($userEmail, FILTER_VALIDATE_EMAIL)){
-
-        $serverName = "localhost\sqlexpress";	      
-        $connectionInfo = array("Database"=>"Test", "UID"=>"ben", "PWD"=>"password123");
+        //Connect to database
+        $serverName = "localhost\sqlexpress";
+        $connectionInfo = array("Database"=>"Test2", "UID"=>"ben", "PWD"=>"password123");
         $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-        $query = "INSERT INTO People VALUES ('$userFirstName', '$userLastName', '$userEmail', '$userPhoneNumber', '$userGender')";
-        $writeToDatabase = sqlsrv_query($conn, $query);
+        //Query database for $username
+        $isUsernameTakenQuery = "SELECT * FROM Users WHERE username = '$username' ";
+        $isUsernameTaken = sqlsrv_query($conn, $isUsernameTakenQuery);
+        
+        //Convert result to boolean
+        $isUsernameTaken = sqlsrv_fetch($isUsernameTaken);
 
-        if($conn){
-            #echo "Connection established.<br><br>";
+        if ($isUsernameTaken == true){
+            $usernameError = "Username taken";
+            $errorStatus = true;
         }
-        else{
-            echo "Connection could not be established.<br><br>";
-            die(print_r(sqlsrv_errors(), true));
-        }
-
-        if ($writeToDatabase){
-            echo "Thank you. Your submission has been recorded.";
-        }
-        else {
-            echo "Error writing to database.<br><br>";
-            die(print_r(sqlsrv_errors(), true));
-        }
-    
-        header("Location:form.html");
     }
+
+    $password = $_POST["password"];
+    if (empty($password)){
+        $passwordError = "Please enter a password.";
+        $errorStatus = true;
+    }
+    $confirmPassword = $_POST["confirmPassword"];
+    if (empty($confirmPassword)){
+        $confirmPasswordError = "Please confirm password.";
+        $errorStatus = true;
+    }
+        if ($confirmPassword != $password){
+        $confirmPasswordError = "Passwords must match.";
+        $errorStatus = true;
+    }
+    if ($errorStatus == false){
+
+        //To calculate new user ID, count number of rows in database and add 1
+        $countExistingUsersQuery = "SELECT * FROM Users";
+        $countExistingUsers = sqlsrv_query($conn, $countExistingUsersQuery, array(), array( "Scrollable" => 'static' ));
+        $row_count = sqlsrv_num_rows( $countExistingUsers );
+        $newUserID = $row_count + 1;
+
+        //Write new account to database
+        $userRegisterQuery = "INSERT INTO Users VALUES ('$newUserID', '$username', '$password', '$timestamp')";
+        $writeToDatabase = sqlsrv_query($conn, $userRegisterQuery);
+
+        //Error messages 
+        if (!$conn){
+            $errorStatus = true;
+            $submitError = "Error connecting to database.";
+        }
+        if (!$writeToDatabase){
+            $errorStatus = true;
+            $submitError = "Error writing to database.";
+            //print_r(sqlsrv_errors());
+        }
+        if ($errorStatus == false){
+            //Open success page
+            header("Location:success.html");
+        }
+
+
+    }
+    //print_r(sqlsrv_errors());
 }
+
 ?>
 
 <html>
 <head>
-<title>Contact Form</title>
-
+<title>Register account</title>
 <link rel="stylesheet" type="text/css" href="default.css">
-
 </head>
+
 <body>
 <center>
 
 <div class="header">
-    <h1>Contact Form</h1>
+    <h1><a href="index.php">Register account</a></h1>
 </div>
 
 <div class="content">
-    Enter your contact details:<br><br>
-    <form class="contact-form" action="? echo htmlspecialchars($_SERVER["PHP_SELF"]) method="post">
+<form class="register-form action="? echo $_SERVER["PHP_SELF"]" method="post">
+<input type="text" name="username" placeholder="Username" value="<?php echo htmlentities($username) ?>"><br>
+<span class="error"><?php echo "$usernameError" ?></span><br><br>
 
-        <input type="text" name="userFirstName" placeholder="First name" value="<?php echo htmlentities($userFirstName) ?>">&nbsp;*<br>
-        <span class="error"><?php echo "$firstNameError" ?></span><br><br>
+<input type="password" name="password" placeholder="Password" value="<?php echo htmlentities($password) ?>"><br>
+<span class="error"><?php echo "$passwordError" ?></span><br><br>
 
-        <input type="text" name="userLastName" placeholder="Last name" value="<?php echo htmlentities($userLastName) ?>">&nbsp;*<br>
-        <span class="error"><?php echo "$lastNameError" ?></span><br><br>
+<input type="password" name="confirmPassword" placeholder="Confirm Password" value="<?php echo htmlentities($confirmPassword) ?>"><br>
+<span class="error"><?php echo "$confirmPasswordError" ?></span><br><br>
 
-        <input type="text" name="userEmail" placeholder="E-mail address" value="<?php echo htmlentities($userEmail) ?>">&nbsp;*<br>
-        <span class="error"><?php echo "$emailError" ?></span><br><br>
-
-        <input type="text" name="userPhoneNumber" placeholder="Phone number" value="<?php echo htmlentities($userPhoneNumber) ?>">&nbsp;*<br>
-        <span class="error"><?php echo "$phoneError" ?></span><br><br>
-
-        <input type="radio" id="male" name="userGender" value="M" <?php echo ($userGender=="M")? "checked":""; ?> >
-        <label for="male">Male</label>
-        <input type="radio" id="female" name="userGender" value="F" <?php echo ($userGender=="F")? "checked":""; ?> >
-        <label for="female">Female</label>
-        <input type="radio" id="other" name="userGender" value="X" <?php echo ($userGender=="X")? "checked":""; ?> >
-        <label for="other">Other</label>&nbsp;*<br>
-        <span class="error"><?php echo $genderError ?></span><br><br>
-
-        <input type="submit" value="Submit" name="submit">
-        
-    </form>
+<input type="submit" value="Submit" name="submit"><br>
+<span class="error"><?php echo "$submitError" ?></span>
+</form>
 </div>
 
 </center>
-</body> 
+</body>
 </html>
